2a898c9780dd876e5a062ae80c99b56f2758da1d
Andrew chen <chy996633@qq.com>
2017-06-11 14:59:35 +0800

    second day on ruby

--- first_seen.rb
+++ first_seen.rb
@@ -1,1 +1,15 @@
 puts 'Today, Jason hold a Ruby tech session for us. Hoping it\'s a good start.'
+
+
+#to_a, lines, reverse poem
+poem = "My honeydew has flown from my hand
+And my toast has gone to the
+        moon.
+But when I saw it on television,
+Planting our flag on Halley's
+        comet,
+More still did I want to eat it.
+"
+
+print poem.lines.to_a.reverse
+
