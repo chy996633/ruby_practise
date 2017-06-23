d021d91addf28f36195445895d71e4645cca0895
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-06-23 04:30:04 -0500

    lotting program for ruby group

--- array.rb
+++ array.rb
@@ -1,4 +1,6 @@
-names = ["Abby Xiao","Ben Qu","Doris","Luna Luo","June Ni","Wells Wei","Johann Deng","Simon Shuai","Daneel Cai","Wandy Qu","George Fan"]
+names = ["Abby Xiao","Ben Qu","Doris","Luna Luo","June Ni","Wells Wei","Johann Deng","Simon Shuai","Daneel Cai","Wandy Qu","George Fan","Samuel Chen"]
 
-
+names.sample(names.length).each do |name|
+  puts name 
+end
 
