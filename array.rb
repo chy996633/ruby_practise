a49b87e576a236643a2628c72bdace50d2f75b28
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-06-22 22:54:47 -0500

    use vim plugin :fugitive

---
+++ array.rb
@@ -0,0 +1,4 @@
+names = ["Abby Xiao","Ben Qu","Doris","Luna Luo","June Ni","Wells Wei","Johann Deng","Simon Shuai","Daneel Cai","Wandy Qu","George Fan"]
+
+
+
---
+++ console.rb
@@ -0,0 +1,12 @@
+pattern = Regexp.new(ARGV[0])
+
+file = File.open(ARGV[1])
+
+file.each_line do |line|
+  if pattern =~ line
+    puts line
+  end
+end
+
+file.close
+
